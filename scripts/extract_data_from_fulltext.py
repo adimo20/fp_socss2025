a1d0cb5118c52c5bcca0d0af5abf85a2b81907a2
adimo20 <adrimontag@gmail.com>
2025-05-21 15:30:11 +0200

    Update extract_data_from_fulltext.py

--- scripts/extract_data_from_fulltext.py
+++ scripts/extract_data_from_fulltext.py
@@ -29,10 +29,7 @@ chat = model.start_chat(history=[])
 
 output_list = []
 results = []
-prompt = "Your porpouse is it to extract a pre-defined part of the text i will provide you. The text i'll provide you is an ocr read" \
-"historical newspaper. Your task is it to extract the marriage request from the text. It is possible that there are more than one in the text" \
-"in this case extract all of them. If you are done, reevaluate your work and check your results. Return the results in the format of a python list." \
-"The response should be just the text you extracted. It just should contain a list of the extracted texts, with no changes made to the text."
+prompt = configs["PROMPT"]
 n = 20
 input_texts = df.plainpagefulltext[:n]
 page_ids = df.page_id[:n]
