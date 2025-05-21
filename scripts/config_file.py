9dab7c83e377ff81d12ab6ef4540929e1011e2f2
adimo20 <adrimontag@gmail.com>
2025-05-21 15:29:55 +0200

    Create config_file.py

---
+++ scripts/config_file.py
@@ -0,0 +1,7 @@
+configs = {
+  "API_KEY" : "API-Key hier in diesen String schreiben",
+  "PROMPT" : "Your porpouse is it to extract a pre-defined part of the text i will provide you. The text i'll provide you is an ocr read" \
+  "historical newspaper. Your task is it to extract the marriage request from the text. It is possible that there are more than one in the text" \
+  "in this case extract all of them. If you are done, reevaluate your work and check your results. Revavluate if the extracted text is really a marriage request and not some sort of other requst. Return the results in the format of a python list." \
+  "The response should be just the text you extracted. It just should contain a list of the extracted texts, with no changes made to the text."
+}
