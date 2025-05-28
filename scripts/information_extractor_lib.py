bba9da51b33f0ab546c8bb17f5bda14c136e4792
adimo20 <adrimontag@gmail.com>
2025-05-28 16:18:18 +0200

    Update information_extractor_lib.py

--- scripts/information_extractor_lib.py
+++ scripts/information_extractor_lib.py
@@ -125,40 +125,44 @@ class OcrCorrecter:
         self.model_name:str = model_name
         self.prompt:str = configs["PROMPT"]
         self.model:str = None
-        self.df = df
-        self.string_for_ocr_correction = None
-        self.prompt_for_ocr_correction = configs["prompt_ocr_correction"]
-        self.initialized_model = False
-        self.lower_boundary = None
-        self.upper_boundary = None
-        self.page_id_colname =page_id_colname
-        self.text_colname = text_colname
+        self.df: pd.DataFrame = df
+        self.string_for_ocr_correction:str = None
+        self.prompt_for_ocr_correction:str = configs["prompt_ocr_correction"]
+        self.initialized_model:bool = False
+        self.lower_boundary:int = None
+        self.upper_boundary:int = None
+        self.page_id_colname:str =page_id_colname
+        self.text_colname:str = text_colname
     
-    def set_config(self):
+    def set_config(self) -> None:
         """Setzen des API Keys, aus dem config_file"""
         API_KEY = configs["API_KEY"]
         genai.configure(api_key=API_KEY)
 
-    def load_model(self):
+    def load_model(self) -> None:
         """Laden des Models und setzen der Modelkonfigurationen"""
         self.model = genai.GenerativeModel(
             model_name=self.model_name,
             generation_config=generation_config)
         self.initialized_model = True
 
-    def create_batch_boundries(self):
-        batch_size = 25
+    def create_batch_boundries(self) -> None:
+        batch_size = configs["batch_size"]
         lower_boundary = [i for i in range(0,len(self.df)) if i % batch_size == 0]
         self.upper_boundary = [i for i in range(0,len(self.df)) if i % batch_size == 0][1:]
         self.lower_boundary = lower_boundary[:len(lower_boundary)-1]
 
-    def create_json_str(self, df, lower, upper):    
+    def create_json_str(self, df, lower, upper) -> str:
+        """Creates a json format string containing the keys - page_id and values text - the ocr-error
+        texts. It is as big as the batch size"""    
         j_list = [{ p : t} for p, t in zip(df[self.page_id_colname], df[self.text_colname])]
         test_list = j_list[lower:upper]
         test_list_str = json.dumps(test_list)
         return test_list_str
         
-    def correct_ocr(self):
+    def correct_ocr(self) -> str:
+        """Sends a string in json format contaning the ocr-error texts and the given page_ids
+        as keys to the api and receives a corrected version of the json string."""
         response = self.model.generate_content([self.prompt_for_ocr_correction, self.string_for_ocr_correction])
         patterns_to_remove = ["```json", "```", "\n"]
         response_text = response.text
@@ -167,7 +171,11 @@ class OcrCorrecter:
 
         self.ocr_corrected_json = response_text
     
-    def json_to_df(self):
+    def json_to_df(self) -> pd.DataFrame:
+
+        """Turns the json-output from api response to a dataframe - at first it 
+        seperates the the keys from the values and the saves both i seprate columns.
+        Meaning a df that contains the columns page_id and text."""
         
         def get_value_key(r):
             value = r.values()
@@ -187,15 +195,15 @@ class OcrCorrecter:
         corrected_df = pd.DataFrame(data={"page_id" : page_ids, "texts": texts})
         return corrected_df     
     
-    def ocr_correction_flow(self):
+    def ocr_correction_flow(self) -> pd.DataFrame:
         """Workflow für die Ocr-Correction!"""
         flow = [self.set_config, self.load_model, self.correct_ocr, self.json_to_df]
 
         for i, f in enumerate(flow):
-            print(f"{f} is done!", flush=True)
             
             if self.initialized_model and i < 2: continue 
-
+            print(f"{f} is done!", flush=True)
+            
             if i < 3:
                 f()
             else:
@@ -203,7 +211,7 @@ class OcrCorrecter:
                 print(f"{f} is done!", flush=True)
                 return result
     
-    def run_batched_correction_flow(self):
+    def run_batched_correction_flow(self) -> None:
         self.create_batch_boundries()
         res_list = []   
         for l, u in zip(self.lower_boundary, self.upper_boundary):
@@ -216,19 +224,16 @@ class OcrCorrecter:
                 res_df.to_csv("corrected_df.csv", sep=";")
 
 
-
-
-        
-    
-
-
-
 if __name__ == "__main__":
     extractor = information_extractor(input_path="newspaper_concat.csv", output_path="page_id_plus_texts.csv", model_name="gemini-1.5-flash")
     extractor.information_extraction_flow()
+
+    df = pd.read_csv("page_id_plus_texts.csv", converters={"page_id_plus_texts":ast.literal_eval}, sep = ";")
+    df["page_id"], df["text"] = df.page_id_plus_texts.apply(lambda l: str(l[0])), df.page_id_plus_texts.apply(lambda l: l[1])
+    ocr_cor = OcrCorrecter(model_name="gemini-1.5-flash", df=df, page_id_colname="page_id", text_colname="text")
+    ocr_cor.run_batched_correction_flow()
+
     
 
 
 
-
-    
