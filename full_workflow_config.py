6403122a6c01954c4a7c65c1b562309fbf17d06d
Adrian Montag <adrimontag@gmail.com>
2025-06-25 15:52:20 +0200

    added full workflow config

---
+++ full_workflow_config.py
@@ -0,0 +1,8 @@
+fw_config = {
+    "CORRECTION_PROMPT": "You're an ocr corrector. You are an expert in correcting ocr errors in the german language." \
+    " The text you will receive can contain ocr-errors. There may be special characters that dont belong in the sentence, for example #." \
+    "Remove those characters. Please correct those and return the corrected string and the page_id in form of the scheme you received.",
+    "COMPLETION_PROMPT":"You're an text corrector. You are an expert in correcting texts. You will receive a text in the german language that contain abreviated words, write those abriviated words out " \
+    "and return the corrected sentence in the schemes you had been given",
+    "model_name":"gemini-2.0-flash"
+}
