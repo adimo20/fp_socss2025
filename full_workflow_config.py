dcd6c1903637b49c02c794c8194f41aee149143f
Adrian Montag <adrimontag@gmail.com>
2025-09-27 14:37:08 +0200

    neueste Version

--- full_workflow_config.py
+++ full_workflow_config.py
@@ -3,6 +3,6 @@ fw_config = {
     " The text you will receive can contain ocr-errors. There may be special characters that dont belong in the sentence, for example #." \
     "Remove those characters. Please correct those and return the corrected string and the page_id in form of the scheme you received.",
     "COMPLETION_PROMPT":"You're an text corrector. You are an expert in correcting texts. You will receive a text in the german language that contain abreviated words, "
-    "write those abriviated words out and return the corrected sentence in the schemes you had been given",
-    "model_name":"gemini-2.0-flash"
+    "write those abriviated words out and return the corrected sentence in the schemes you had been given. If there are no abriviated words return the sentence as it is.",
+    "model_name":"gemini-2.5-flash-lite"
 }
